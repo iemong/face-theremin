9cb923b8a4fe2d01896c8dd9ecf9e5790af88655
iemong <kiyoshidainagon@gmail.com>
2024-10-01 23:56:05 +0900

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import { Button } from "@/components/ui/button.tsx";
+import { useToast } from "@/hooks/use-toast.ts";
+import { useAudio } from "@/hooks/use-audio.ts";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [permissionStatus, setPermissionStatus] =
+    useState<PermissionState>("prompt");
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const { toast } = useToast();
+
+  const setupCamera = async () => {
+    try {
+      await checkPermissionStatus();
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
+        setPermissionStatus("granted");
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      }
+    } catch (e) {
+      console.log(e);
+      if (e instanceof Error && e.name === "NotAllowedError") {
+        toast({
+          title: "カメラの使用が許可されていません",
+        });
+        setPermissionStatus("denied");
+      }
+    }
+  };
+
+  const checkPermissionStatus = async () => {
+    try {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      const status = await navigator.permissions.query({ name: "camera" });
+      console.log(status);
+      setPermissionStatus(status.state);
+      status.onchange = () => setPermissionStatus(status.state);
+    } catch (e) {
+      console.error("Permission API is not supported", e);
+    }
+  };
+
+  const { isPlaying, setupAudio, playAudio, stopAudio } = useAudio();
+
+  const handleSetup = async () => {
+    await setupCamera();
+    setupAudio();
+  };
+
+  useEffect(() => {
+    checkPermissionStatus();
+  }, []);
+
+  const renderPermissionInstructions = () => {
+    switch (permissionStatus) {
+      case "denied":
+        return (
+          <div
+            className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+            role="alert"
+          >
+            <strong className="font-bold">
+              カメラへのアクセスが拒否されています。
+            </strong>
+            <p className="block sm:inline">
+              ブラウザの設定でカメラへのアクセスを許可してください。
+            </p>
+            <p>
+              設定方法:
+              <ol className="list-decimal list-inside">
+                <li>
+                  ブラウザのアドレスバーの左側にあるカメラアイコンをクリック
+                </li>
+                <li>「常に許可」を選択</li>
+                <li>ページをリロード</li>
+              </ol>
+            </p>
+          </div>
+        );
+      case "prompt":
+        return (
+          <p className="text-yellow-600">
+            アプリを開始すると、カメラへのアクセス許可を求められます。
+          </p>
+        );
+      default:
+        return null;
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className={"w-screen h-screen"}>
+      {renderPermissionInstructions()}
+      <video ref={videoRef} autoPlay muted className={"object-cover"} />
+      <Button onClick={handleSetup}>Play</Button>
+      {isPlaying ? (
+        <Button onClick={stopAudio}>Stop</Button>
+      ) : (
+        <Button onClick={playAudio}>Play</Button>
+      )}
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/hooks/use-audio.ts
@@ -0,0 +1,54 @@
+import { useCallback, useState } from "react";
+
+export const useAudio = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [audioContext, setAudioContext] = useState<AudioContext | undefined>(
+    undefined,
+  );
+  const [oscillator, setOscillator] = useState<OscillatorNode | undefined>(
+    undefined,
+  );
+
+  const createOscillator = useCallback((context: AudioContext) => {
+    const osc = context.createOscillator();
+    osc.type = "sine";
+    osc.connect(context.destination);
+    return osc;
+  }, []);
+
+  const setupAudio = useCallback(() => {
+    const context = new window.AudioContext();
+    const osc = createOscillator(context);
+    osc.start();
+    setIsPlaying(true);
+    setAudioContext(context);
+    setOscillator(osc);
+  }, [createOscillator]);
+
+  const playAudio = useCallback(() => {
+    if (oscillator !== undefined || isPlaying) {
+      return;
+    }
+    const osc = createOscillator(audioContext!);
+    osc.start();
+    setIsPlaying(true);
+    setOscillator(osc);
+  }, [audioContext, createOscillator, isPlaying, oscillator]);
+
+  const stopAudio = useCallback(() => {
+    if (oscillator === undefined || !isPlaying) {
+      return;
+    }
+    oscillator.stop();
+    oscillator.disconnect(audioContext!.destination);
+    setIsPlaying(false);
+    setOscillator(undefined)
+  }, [audioContext, isPlaying, oscillator]);
+
+  return {
+    isPlaying,
+    setupAudio,
+    playAudio,
+    stopAudio,
+  };
+};
